d9131ce91a69fbf19494c864e48a51c436ca8d4e
Dries Goossens <driesgoossens93@gmail.com>
2018-06-04 20:43:56 +0200

    adding more errors

--- afspraak_backend.php
+++ afspraak_backend.php
@@ -1,4 +1,10 @@
 <?php
+error_reporting(E_ALL);
+
+ini_set(‘display_errors’, TRUE);
+
+ini_set(‘display_startup_errors’, TRUE);
+
 require_once '/home/borahv1q/etc/google-api-php-client/src/Google/autoload.php';
 
 putenv('GOOGLE_APPLICATION_CREDENTIALS=/home/borahv1q/public_html/service_account.json');
