43c7bf8b3adfdd51717030f5b30319c24aeb7c6c
Dries Goossens <driesgoossens93@gmail.com>
2018-06-04 19:31:44 +0200

    split html & php afspraak
    split html & php afspraak

---
+++ afspraak_backend.php
@@ -0,0 +1,32 @@
+<?php
+require_once '/home/borahv1q/etc/google-api-php-client/src/Google/autoload.php';
+
+if (isset($_POST['action'])) {
+    switch ($_POST['action']) {
+        case 'insert':
+            insert();
+            break;
+        case 'select':
+            select();
+            break;
+    }
+}
+function insert() {
+    echo "The insert function is called.";
+	
+	putenv('GOOGLE_APPLICATION_CREDENTIALS=/home/borahv1q/public_html/service_account.json');
+
+	$client = new Google_Client();
+	$client->useApplicationDefaultCredentials();
+
+	$sqladmin = new Google_Service_SQLAdmin($client);
+	$response = $sqladmin->instances
+		->listInstances('challenger')->getItems();
+	echo json_encode($response) . "\n";
+	
+    exit;
+}
+
+
+
+?>
