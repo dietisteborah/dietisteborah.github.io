1acd265757503493b66e16a1134b30c98e843e62
Dries Goossens <driesgoossens93@gmail.com>
2018-06-04 20:55:38 +0200

    easy test .php

--- afspraak.php
+++ afspraak.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
 <html>
-<body>
-
-<?php
-error_reporting(E_ALL);
-
-ini_set(‘display_errors’, TRUE);
-
-ini_set(‘display_startup_errors’, TRUE);
-
-function familyName($fname, $year) {
-    echo "$fname Refsnes. Born in $year <br>";
-}
-
-familyName("Hege","1975");
-familyName("Stale","1978");
-familyName("Kai Jim","1983");
-?>
-
-</body>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
 </html>
