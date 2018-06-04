9ae9fa42bec71c4a9d028bf0ca34d9ecd4762960
Dries Goossens <driesgoossens93@gmail.com>
2018-06-04 19:39:45 +0200

    test

--- afspraak_backend.php
+++ afspraak_backend.php
@@ -17,19 +17,9 @@ function select() {
     echo "The select function is called.";
     exit;
 }
+
 function insert() {
     echo "The insert function is called.";
-	
-	putenv('GOOGLE_APPLICATION_CREDENTIALS=/home/borahv1q/public_html/service_account.json');
-
-	$client = new Google_Client();
-	$client->useApplicationDefaultCredentials();
-
-	$sqladmin = new Google_Service_SQLAdmin($client);
-	$response = $sqladmin->instances
-		->listInstances('challenger')->getItems();
-	echo json_encode($response) . "\n";
-	
     exit;
 }
 
