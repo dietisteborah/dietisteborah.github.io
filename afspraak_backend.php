af7671aa836ecbf0411b9b0a627d40365ccf7445
Dries Goossens <driesgoossens93@gmail.com>
2018-06-04 21:44:45 +0200

    move php to dedicated file

--- afspraak_backend.php
+++ afspraak_backend.php
@@ -1,20 +1,46 @@
 <?php
-error_reporting(E_ALL);
+	error_reporting(E_ALL);
 
-ini_set(‘display_errors’, TRUE);
+	require_once '/home/borahv1q/dietisteborah.github.io/vendor/autoload.php';
 
-ini_set(‘display_startup_errors’, TRUE);
+	putenv('GOOGLE_APPLICATION_CREDENTIALS=/home/borahv1q/public_html/service_account.json');
+	
+	if (isset($_POST['action'])) {
+		switch ($_POST['action']) {
+			case 'authAPI':
+				insert();
+				break;
+		}
+	}	
+	
+	function authAPI(){
+		$client = new Google_Client();
+		$client->useApplicationDefaultCredentials();
+		$client->setScopes(array(
+			'https://www.googleapis.com/auth/calendar.readonly'
+		));
+		$service = new Google_Service_Calendar($client);
+		// Print the next 10 events on the user's calendar.
+		$calendarId = 'getchallengedapp@gmail.com';
+		$optParams = array(
+		  'maxResults' => 10,
+		  'orderBy' => 'startTime',
+		  'singleEvents' => true,
+		  'timeMin' => date('c'),
+		);
+		$results = $service->events->listEvents($calendarId, $optParams);
 
-phpinfo();
-
-require_once '/home/borahv1q/etc/google-api-php-client/src/Google/autoload.php';
-
-putenv('GOOGLE_APPLICATION_CREDENTIALS=/home/borahv1q/public_html/service_account.json');
-
-$client = new Google_Client();
-$client->useApplicationDefaultCredentials();
-
-$sqladmin = new Google_Service_SQLAdmin($client);
-$response = $sqladmin->instances->listInstances('challenger')->getItems();
-echo json_encode($response) . "\n";
+		if (empty($results->getItems())) {
+			print "No upcoming events found.\n";
+		} else {
+			print "Upcoming events:\n";
+			foreach ($results->getItems() as $event) {
+				$start = $event->start->dateTime;
+				if (empty($start)) {
+					$start = $event->start->date;
+				}
+				printf("%s (%s)\n", $event->getSummary(), $start);
+			}
+		}
+	}
 ?>
