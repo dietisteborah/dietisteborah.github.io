fbab62ae229be7a5189a341233d64febbeef25df
Dries Goossens <driesgoossens93@gmail.com>
2018-06-04 20:03:04 +0200

    test

--- afspraak_backend.php
+++ afspraak_backend.php
@@ -1,28 +1,13 @@
 <?php
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
 require_once '/home/borahv1q/etc/google-api-php-client/src/Google/autoload.php';
 
-if (isset($_POST['action'])) {
-    switch ($_POST['action']) {
-        case 'insert':
-            insert();
-            break;
-        case 'select':
-            select();
-            break;
-    }
-}
-function select() {
-    echo "The select function is called.";
-    exit;
-}
+putenv('GOOGLE_APPLICATION_CREDENTIALS=/home/borahv1q/public_html/service_account.json');
 
-function insert() {
-    echo "The insert function is called.";
-    exit;
-}
+$client = new Google_Client();
+$client->useApplicationDefaultCredentials();
 
-
-
+$sqladmin = new Google_Service_SQLAdmin($client);
+$response = $sqladmin->instances
+    ->listInstances('challenger')->getItems();
+echo json_encode($response) . "\n";
 ?>
