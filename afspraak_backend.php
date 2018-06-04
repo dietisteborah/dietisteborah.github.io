1f98fab04b8e25bf6fcce5ba207c3fb68ca0d881
Dries Goossens <driesgoossens93@gmail.com>
2018-06-04 20:46:40 +0200

    added phpinfo

--- afspraak_backend.php
+++ afspraak_backend.php
@@ -5,6 +5,8 @@ ini_set(‘display_errors’, TRUE);
 
 ini_set(‘display_startup_errors’, TRUE);
 
+phpinfo();
+
 require_once '/home/borahv1q/etc/google-api-php-client/src/Google/autoload.php';
 
 putenv('GOOGLE_APPLICATION_CREDENTIALS=/home/borahv1q/public_html/service_account.json');
@@ -13,7 +15,6 @@ $client = new Google_Client();
 $client->useApplicationDefaultCredentials();
 
 $sqladmin = new Google_Service_SQLAdmin($client);
-$response = $sqladmin->instances
-    ->listInstances('challenger')->getItems();
+$response = $sqladmin->instances->listInstances('challenger')->getItems();
 echo json_encode($response) . "\n";
 ?>
