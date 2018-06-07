d041995ae1a2dfba61715941ab641a387ed2ef1d
Dries Goossens <driesgoossens93@gmail.com>
2018-06-07 17:09:29 +0200

    usage of hello-week

--- afspraak_backend.php
+++ afspraak_backend.php
@@ -14,7 +14,7 @@ class Calendar {
     }
      
     /********************* PROPERTY ********************/  
-    private $dayLabels = array("Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag","Zondag");
+    private $dayLabels = array("Ma","Di","Wo","Do","Vr","Za","Zo");
      
     private $currentYear=0;
      
@@ -27,8 +27,6 @@ class Calendar {
     private $daysInMonth=0;
      
     private $naviHref= null;
-	
-	private $currentWeek=0;
      
     /********************* PUBLIC **********************/  
         
@@ -36,38 +34,40 @@ class Calendar {
     * print out the calendar
     */
     public function show() {
-         	
-		$week = null;
-        
-		$day = null;
-		
-		$hour = null;               
-
-        if(null==$week&&isset($_GET['week'])){
- 
-            $week = $_GET['week'];
+        $year  == null;
          
-        }else if(null==$week){
- 
-            $week = time();
+        $month == null;
          
-        }  	
-        if(null==$hour&&isset($_GET['hour'])){
-			$timeArray = explode ( '-' , $_GET['hour'] );
-            $hour = $timeArray[0];
-			$week = $timeArray[1];
-        }else if(null==$hour){
+        if(null==$year&&isset($_GET['year'])){
  
-            $hour = date("h",time());
+            $year = $_GET['year'];
          
-        }  			
-                 
-		$this->currentWeek=$week;
-		/*
+        }else if(null==$year){
+ 
+            $year = date("Y",time());  
+         
+        }          
+         
+        if(null==$month&&isset($_GET['month'])){
+ 
+            $month = $_GET['month'];
+         
+        }else if(null==$month){
+ 
+            $month = date("m",time());
+         
+        }                  
+         
+        $this->currentYear=$year;
+         
+        $this->currentMonth=$month;
+         
+        $this->daysInMonth=$this->_daysInMonth($month,$year);  
+         
         $content='<div id="calendar">'.
-						'<div class="box">'.
-						$this->_createNaviWeek().
-						'</div>'.
+                        '<div class="box">'.
+                        $this->_createNavi().
+                        '</div>'.
                         '<div class="box-content">'.
                                 '<ul class="label">'.$this->_createLabels().'</ul>';   
                                 $content.='<div class="clear"></div>';     
@@ -89,32 +89,6 @@ class Calendar {
              
                         $content.='</div>';
                  
-        $content.='</div>'; */
-		
-		        $content='<div id="calendar">'.
-						'<div class="box">'.
-						$this->_createNaviWeek().
-						'</div>'.
-                        '<div class="box-content">'.
-                                '<ul class="label">'.$this->_createLabels().'</ul>';   
-                                $content.='<div class="clear"></div>';     
-                                $content.='<ul class="dates">';    
-                                 
-                                // Create hours in a day
-                                for( $i=7; $i<22; $i++ ){
-                                     
-                                    //Create days in a week
-                                    for($j=1;$j<=7;$j++){
-                                        $content.=$this->_showHour($i,$j);
-                                    }
-                                }
-                                 
-                                $content.='</ul>';
-                                 
-                                $content.='<div class="clear"></div>';     
-             
-                        $content.='</div>';
-                 
         $content.='</div>';
         return $content;   
     }
@@ -123,7 +97,6 @@ class Calendar {
     /**
     * create the li element for ul
     */
-	/*
     private function _showDay($cellNumber){
          
         if($this->currentDay==0){
@@ -155,24 +128,11 @@ class Calendar {
          
         return '<li id="li-'.$this->currentDate.'" class="'.($cellNumber%7==1?' start ':($cellNumber%7==0?' end ':' ')).
                 ($cellContent==null?'mask':'').'">'.$cellContent.'</li>';
-    }*/
-	
-    private function _showHour($hour,$day){    
-		/*z$wday = date('w', $this->currentWeek);
-		$ID = $hour."-".date('d-m-Y', $this->currentWeek - ($wday - $day)*86400);
-        //$ID= $hour.$day;
-        //return '<li id="li-'.$ID.'" href="'.$this->naviHref.'?hour='.sprintf('%03d',$ID).'">'.$hour.'</li>';
-		return '<li> <a href="'.$this->naviHref.'?hour='.$ID.'">'.$hour.'</a></li>';
-		*/
-		$ID = $hour."-".$this->currentWeek;
-        //$ID= $hour.$day;
-        //return '<li id="li-'.$ID.'" href="'.$this->naviHref.'?hour='.sprintf('%03d',$ID).'">'.$hour.'</li>';
-		return '<li> <a href="'.$this->naviHref.'?hour='.$ID.'">'.$hour.'</a></li>';
-    } 
+    }
+     
     /**
     * create navigation
     */
-	/*
     private function _createNavi(){
          
         $nextMonth = $this->currentMonth==12?1:intval($this->currentMonth)+1;
@@ -189,28 +149,12 @@ class Calendar {
                     '<span class="title">'.date('Y M',strtotime($this->currentYear.'-'.$this->currentMonth.'-1')).'</span>'.
                 '<a class="next" href="'.$this->naviHref.'?month='.sprintf("%02d", $nextMonth).'&year='.$nextYear.'">Next</a>'.
             '</div>';
-    }*/
-    /**
-    * create week navigation
-    */
-    private function _createNaviWeek(){
+    }
          
-		$nextWeek = $this->currentWeek+(7*86400);
-		$preWeek = $this->currentWeek-(7*86400);
-        $wday = date('w', $this->currentWeek); 
-        return
-            '<div class="header">'.
-                '<a class="prev" href="'.$this->naviHref.'?week='.sprintf('%02d',$preWeek).'">Prev</a>'.
-                    '<span class="title">'.date('d-m-Y', $this->currentWeek - ($wday - 1)*86400).'</span>'.
-                '<a class="next" href="'.$this->naviHref.'?week='.sprintf("%02d", $nextWeek).'">Next</a>'.
-            '</div>';
-			
-    }         
     /**
     * create calendar week labels
     */
-	/*
-	private function _createLabels(){  
+    private function _createLabels(){  
                  
         $content='';
          
@@ -222,22 +166,52 @@ class Calendar {
          
         return $content;
     }
-	*/
-    private function _createLabels(){  
-                 
-        $content='';
-        $counter=1; 
-		$wday = date('w', $this->currentWeek); 
-        foreach($this->dayLabels as $index=>$label){
-            $daydate=$label;
-			$daydate.=" ";
-			$daydate.=date('d/m', $this->currentWeek - ($wday - $counter)*86400); 
-            $content.='<li>'.$daydate.'</li>';
-			$counter++;
+     
+     
+     
+    /**
+    * calculate number of weeks in a particular month
+    */
+    private function _weeksInMonth($month=null,$year=null){
+         
+        if( null==($year) ) {
+            $year =  date("Y",time()); 
         }
          
-        return $content;
+        if(null==($month)) {
+            $month = date("m",time());
+        }
+         
+        // find number of days in this month
+        $daysInMonths = $this->_daysInMonth($month,$year);
+         
+        $numOfweeks = ($daysInMonths%7==0?0:1) + intval($daysInMonths/7);
+         
+        $monthEndingDay= date('N',strtotime($year.'-'.$month.'-'.$daysInMonths));
+         
+        $monthStartDay = date('N',strtotime($year.'-'.$month.'-01'));
+         
+        if($monthEndingDay<$monthStartDay){
+             
+            $numOfweeks++;
+         
+        }
+         
+        return $numOfweeks;
     }
-
+ 
+    /**
+    * calculate number of days in a particular month
+    */
+    private function _daysInMonth($month=null,$year=null){
+         
+        if(null==($year))
+            $year =  date("Y",time()); 
+ 
+        if(null==($month))
+            $month = date("m",time());
+             
+        return date('t',strtotime($year.'-'.$month.'-01'));
+    }
      
 }
