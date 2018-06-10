96d1bfbf99faa97b101e9a2d96555cc383c1f2f2
Dries Goossens <driesgoossens93@gmail.com>
2018-06-10 16:04:22 +0200

    added google calendar api php

---
+++ php/afspraken_backend.php
@@ -0,0 +1,42 @@
+<?php
+	require_once '/home/borahv1q/etc/google-api-php-client/src/Google/autoload.php';
+	putenv('GOOGLE_APPLICATION_CREDENTIALS=/home/borahv1q/public_html/service_account.json');
+	
+	if (isset($_POST['action'])) {
+		switch ($_POST['action']) {
+			case 'authAPI':
+				insert();
+				break;
+		}
+	}	
+	
+	function authAPI(){
+		$client = new Google_Client();
+		$client->useApplicationDefaultCredentials();
+		$client->setScopes(array(
+			'https://www.googleapis.com/auth/calendar.readonly'
+		));
+		$service = new Google_Service_Calendar($client);
+		// Print the next 10 events on the user's calendar.
+		$calendarId = 'dietiste.borah@gmail.com';
+		$optParams = array(
+		  'maxResults' => 10,
+		  'orderBy' => 'startTime',
+		  'singleEvents' => true,
+		  'timeMin' => date('c'),
+		);
+		$results = $service->events->listEvents($calendarId, $optParams);
+		if (empty($results->getItems())) {
+			print "No upcoming events found.\n";
+		} else {
+			print "Upcoming events:\n";
+			foreach ($results->getItems() as $event) {
+				$start = $event->start->dateTime;
+				if (empty($start)) {
+					$start = $event->start->date;
+				}
+				printf("%s (%s)\n", $event->getSummary(), $start);
+			}
+		}
+	}
+?>
