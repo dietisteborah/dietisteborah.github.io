62ca3553091be6ae2a61ea5b82f003ad1097605b
Dries Goossens <driesgoossens93@gmail.com>
2018-06-11 20:45:45 +0200

    generate token

--- afspraken_backend.php
+++ afspraken_backend.php
@@ -21,10 +21,23 @@
 		$client->setAccessType('offline');
 
 		// Load previously authorized credentials from a file.
-		$credentialsPath = '/home/borahv1q/public_html/credentials.json';
-		if (file_exists($credentialsPath)) {
-			$accessToken = json_decode(file_get_contents($credentialsPath), true);
-		} 
+
+			// Request authorization from the user.
+			$authUrl = $client->createAuthUrl();
+			printf("Open the following link in your browser:\n%s\n", $authUrl);
+			print 'Enter verification code: ';
+			$authCode = trim(fgets(STDIN));
+
+			// Exchange authorization code for an access token.
+			$accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+
+			// Store the credentials to disk.
+			if (!file_exists(dirname($credentialsPath))) {
+				mkdir(dirname($credentialsPath), 0700, true);
+			}
+			file_put_contents($credentialsPath, json_encode($accessToken));
+			printf("Credentials saved to %s\n", $credentialsPath);
+
 		$client->setAccessToken($accessToken);
 
 		// Refresh the token if it's expired.
