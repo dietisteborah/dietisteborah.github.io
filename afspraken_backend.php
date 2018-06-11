975de1fe906283e33af1431ee639b16732f68401
Dries Goossens <driesgoossens93@gmail.com>
2018-06-11 20:33:31 +0200

    test with offline account

--- afspraken_backend.php
+++ afspraken_backend.php
@@ -11,15 +11,50 @@
 				break;
 		}
 	}	
+
+	function getClient()
+	{
+		$client = new Google_Client();
+		$client->setApplicationName('Dietiste Borah');
+		$client->setScopes(Google_Service_Calendar::CALENDAR_READONLY);
+		$client->setAuthConfig('/home/borahv1q/public_html/client_secret.json');
+		$client->setAccessType('offline');
+
+		// Load previously authorized credentials from a file.
+		$credentialsPath = '/home/borahv1q/public_html/credentials.json';
+		if (file_exists($credentialsPath)) {
+			$accessToken = json_decode(file_get_contents($credentialsPath), true);
+		} else {
+			// Request authorization from the user.
+			$authUrl = $client->createAuthUrl();
+			printf("Open the following link in your browser:\n%s\n", $authUrl);
+			print 'Enter verification code: ';
+			$authCode = trim(fgets(STDIN));
+
+			// Exchange authorization code for an access token.
+			$accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+
+			// Store the credentials to disk.
+			if (!file_exists(dirname($credentialsPath))) {
+				mkdir(dirname($credentialsPath), 0700, true);
+			}
+			file_put_contents($credentialsPath, json_encode($accessToken));
+			printf("Credentials saved to %s\n", $credentialsPath);
+		}
+		$client->setAccessToken($accessToken);
+
+		// Refresh the token if it's expired.
+		if ($client->isAccessTokenExpired()) {
+			$client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+			file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+		}
+		return $client;
+	}
 	
 	function authAPI(){
-		$client = new Google_Client();
-		$client->setAccessType("offline");
-		$client->useApplicationDefaultCredentials();
-		$client->setScopes(array(
-			'https://www.googleapis.com/auth/calendar.readonly'
-		));
+		$client = getClient();
 		$service = new Google_Service_Calendar($client);
+		
 		// Print the next 10 events on the user's calendar.
 		//$calendarId = 'calendar-service-php@dietiste-calendar-site.iam.gserviceaccount.com';
 		$calendarId = 'dietiste.borah@gmail.com';
