09a7d0793da64bc318f0f779ea5940594f1bff68
Dries Goossens <driesgoossens93@gmail.com>
2018-06-13 19:06:04 +0200

    check time of the day

--- afspraken_backend.php
+++ afspraken_backend.php
@@ -52,8 +52,8 @@
 		//$realdate = date('YYYY-MM-DD', $date)->add(new DateInterval('P1D'));
 		$nextdate = new DateTime($strdate);
 		$nextdate->add(new DateInterval('P1D'));
-		//echo $nextdate->format('Y-m-d') . "\n" . $strdate;
-					
+		echo $nextdate->format('Y-m-d') . "\n" . $strdate . " " . date('H:i:s');
+		/*			
 		$optParams = array(
 		  'maxResults' => 10,
 		  'orderBy' => 'startTime',
@@ -74,6 +74,6 @@
 				printf("%s (%s)\n", $event->getSummary(), $start);
 			}
 		}
-		
+		*/
 	}
 ?>
