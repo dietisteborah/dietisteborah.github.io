3294e074d6470cd61df344fbd0029a7b1e83857a
Dries Goossens <driesgoossens93@gmail.com>
2018-06-13 19:07:18 +0200

    check appointment made in agenda

--- afspraken_backend.php
+++ afspraken_backend.php
@@ -52,14 +52,15 @@
 		//$realdate = date('YYYY-MM-DD', $date)->add(new DateInterval('P1D'));
 		$nextdate = new DateTime($strdate);
 		$nextdate->add(new DateInterval('P1D'));
-		echo $nextdate->format('Y-m-d') . "\n" . $strdate . " " . date('H:i:s');
-		/*			
+		//echo $nextdate->format('Y-m-d') . "\n" . $strdate;
+					
 		$optParams = array(
 		  'maxResults' => 10,
 		  'orderBy' => 'startTime',
 		  'singleEvents' => true,
 		  'timeMax' => $nextdate->format('Y-m-d') . 'T00:00:00Z',
-		  'timeMin' => $strdate . 'T' . date('H:i:s') . 'Z',
+		  'timeMin' => $strdate . 'T00:00:00Z',
+		  //'timeMin' => $strdate . 'T' . date('H:i:s') . 'Z',
 		);
 		$results = $service->events->listEvents($calendarId, $optParams);
 		if (!($results->getItems())) {
@@ -74,6 +75,6 @@
 				printf("%s (%s)\n", $event->getSummary(), $start);
 			}
 		}
-		*/
+		
 	}
 ?>
