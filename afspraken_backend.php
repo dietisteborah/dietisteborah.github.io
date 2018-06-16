fa415054a895896506f08d530754227d2e5789f5
Dries Goossens <driesgoossens93@gmail.com>
2018-06-16 14:34:29 +0200

    bug hunting

--- afspraken_backend.php
+++ afspraken_backend.php
@@ -96,12 +96,12 @@
 						if(!($event->getSummary() == "Open"){
 							//Check begintijd met eind tijd vorige afspraak. Daarna "eindtijd" op eigen eindtijd zetten. 
 							//Op basis daarvan vrije momenten toevoegen aan de lijst met vrije uren (aantal minuten delen door 30 of 90)
-							$startDateTime = $event->start->dateTime;
+							/*$startDateTime = $event->start->dateTime;
 							$start = substr($startDateTime, 11, 5);
 							$end = $event->getEnd()->dateTime;
 							if(strtotime($start) > strtotime($previousEndTime){
 								$timeDifferenceInMinutes = (strtotime($start) - strtotime($previousEndTime))/60;
-							}
+							}*/
 						}
 						printf("%s (%s) \n", $event->getSummary(), $timeDifferenceInMinutes);
 					}
