fb9b1b8c166728d27d09de6a71805de4338cceb1
Dries Goossens <driesgoossens93@gmail.com>
2018-06-21 23:58:16 +0200

    bug

--- afspraken_backend.php
+++ afspraken_backend.php
@@ -220,7 +220,7 @@
 		  'summary' => $name . ' '. $type,
 		  'description' => $name . ' - '.$remark.' - '.$email.' '.$phone.' '.$type,
 		  'start' => array(
-			'dateTime' => $date.'T'.$startTime.':00',
+			'dateTime' => $date.'T'.$time.':00',
 			'timeZone' => 'Europe/Brussels',
 		  ),
 		  'end' => array(
