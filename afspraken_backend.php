5687023959fd34bac4a17d602eb094e7d50c1209
Dries Goossens <driesgoossens93@gmail.com>
2018-06-24 15:02:16 +0200

    update get app function to sql query

--- afspraken_backend.php
+++ afspraken_backend.php
@@ -1,7 +1,7 @@
 <?php
 	require_once '../vendor/autoload.php';
 	putenv('GOOGLE_APPLICATION_CREDENTIALS=/home/borahv1q/borah-secrets/client_secret.json');
-	
+
 	if (isset($_POST['action'])) {
 		switch ($_POST['action']) {
 			case 'getAvailable':
@@ -14,8 +14,8 @@
 				loadToday($_POST['date']);
 				break;
 				}
-	}	
-	
+	}
+
 	function getClient()
 	{
 		$client = new Google_Client();
@@ -40,7 +40,7 @@
 		}
 		return $client;
 	}
-	
+
 	function getAvailable($strdate,$opvolg){
 		$today = new DateTime(); // This object represents current date/time
 		$today->setTime( 0, 0, 0 ); // reset time part, to prevent partial comparison
@@ -48,120 +48,35 @@
 		$match_date->setTime( 0, 0, 0 ); // reset time part, to prevent partial comparison
 		$diff = $today->diff( $match_date );
 		$diffDays = (integer)$diff->format( "%R%a" ); // Extract days count in interval
-		$appType = false;
+		$appType = 0;
 		//check appointment type
 		if($opvolg=="opvolg"){
-			$appType=true;
+			$appType=1;
 		}
 		if($diffDays > 0){
-			$open = false;
-			$startOpen = "";
-			$endOpen = "";
-			$timeslots = array();
 			$notime = true;
-			$client = getClient();
-			$service = new Google_Service_Calendar($client);
-			$calendarId = 'dietiste.borah@gmail.com';
-			
-			//time max -> selected day + 1
-			$nextdate = new DateTime($strdate);
-			$nextdate->add(new DateInterval('P1D'));
-						
-			$optParams = array(
-			  'orderBy' => 'startTime',
-			  'singleEvents' => true,
-			  'timeMax' => $nextdate->format('Y-m-d') . 'T00:00:00Z',
-			  'timeMin' => $strdate . 'T00:00:00Z',
-			);
-			$results = $service->events->listEvents($calendarId, $optParams);
-			if (!($results->getItems())) {
+			//Create database connection
+			$string = file_get_contents("pw.txt");
+			$string = str_replace(array("\r", "\n"), '', $string);
+			$link = mysqli_connect("localhost", "borahv1q", $string , "borahv1q_Agenda");
+			if (!$link) {
+				echo "Error: Unable to connect to MySQL." . PHP_EOL;
+				echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+				echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+				exit;
+			}
+			echo "Connect to mysql.\n" . PHP_EOL;
+
+			$sql = "SELECT * FROM afspraken where date =".$today->fomrat("Y-m-d")." and opvolg =".$appType;
+			$result = mysqli_query($link, $sql);
+			if (mysqli_num_rows($result) > 0) {
+				// output data of each row
+				while($row = mysqli_fetch_assoc($result)) {
+					//echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+					printf("%s;", $row["startTime"]);
+				}
+			} else {
 				print "Geen tijdstippen vrij op deze datum.\n";
-			} else 
-			{
-				foreach ($results->getItems() as $event) {
-					if($event->getSummary() == "Open"){
-						$open=true;
-						$startOpen = $event->start->dateTime;
-						$endOpen = $event->getEnd()->dateTime;
-						break;
-					}
-				}
-				if($open){
-					//Query the events during the opening times
-					$optParams = array(
-					  'orderBy' => 'startTime',
-					  'singleEvents' => true,
-					  'timeMax' => $endOpen,
-					  'timeMin' => $startOpen,
-					);
-					$results = $service->events->listEvents($calendarId, $optParams);
-					
-					$startOpen=substr($startOpen, 11, 5);
-					$previousEndTime = $startOpen; //First time, difference between Open "openingtime" and first appointment has to be found
-					//$opvolg=false;
-					foreach ($results->getItems() as $event) {
-						if(!($event->getSummary() == "Open")){
-							//Check begintijd met eind tijd vorige afspraak. Daarna "eindtijd" op eigen eindtijd zetten. 
-							//Op basis daarvan vrije momenten toevoegen aan de lijst met vrije uren (aantal minuten delen door 30 of 90)
-							$startDateTime = $event->start->dateTime;
-							$start = substr($startDateTime, 11, 5);						
-							if(strtotime($start) > strtotime($previousEndTime)){
-								$timeDifferenceInMinutes = (strtotime($start) - strtotime($previousEndTime))/60;
-								if($appType && ($timeDifferenceInMinutes/30) >= 1){ //afspraak 30 min
-									$noTime = false;
-									$amountOfAppointments = $timeDifferenceInMinutes/30;
-									for($i=0;$i<$amountOfAppointments;$i++){
-										$add = 30 + (30*$i);
-										$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
-										printf("%s;", date("H:i",$newStartTime));
-									}
-								}
-								elseif((!$appType && ($timeDifferenceInMinutes/90) >= 1)){ //afspraak van 90 min
-									$noTime = false;
-									$amountOfAppointments = $timeDifferenceInMinutes/30; //elke 30 min een afspraak
-									for($i=0;$i<($amountOfAppointments-2);$i++){
-										$add = 30 + (30*$i);
-										$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
-										printf("%s;", date("H:i",$newStartTime));
-									}
-								}
-							}
-							else{
-								//Do nothing, no time left
-							}
-							$previousEndTime = substr($event->getEnd()->dateTime,11,5);
-						}
-					}
-					//do the check for the last appointment & closing time
-					$endOpen=substr($endOpen, 11, 5);
-					if(strtotime($endOpen) > strtotime($previousEndTime)){
-						$timeDifferenceInMinutes = (strtotime($endOpen) - strtotime($previousEndTime))/60;
-						if($appType && ($timeDifferenceInMinutes/30) >= 1){ //afspraak 30 min
-							$noTime = false;
-							$amountOfAppointments = $timeDifferenceInMinutes/30;
-							for($i=0;$i<$amountOfAppointments;$i++){
-								$add = 30 + (30*$i);
-								$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
-								printf("%s;", date("H:i",$newStartTime));
-							}
-						}
-						elseif((!$appType && ($timeDifferenceInMinutes/90) >= 1)){ //afspraak van 90 min
-							$noTime = false;
-							$amountOfAppointments = $timeDifferenceInMinutes/30; //elke 30 min een afspraak
-							for($i=0;$i<($amountOfAppointments-2);$i++){
-								$add = 30 + (30*$i);
-								$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
-								printf("%s;", date("H:i",$newStartTime));
-							}
-						}
-					}		
-					if($noTime){
-						print "Geen tijdstippen vrij op deze datum.\n";
-					}
-				}
-				else{
-					print "Geen tijdstippen vrij op deze datum.\n";
-				}
 			}
 		}
 		else{
@@ -174,7 +89,7 @@
 			if($name == ""){
 				$complete = false;
 				$bericht = "Gelieve jouw naam in te vullen.\n";
-			} 
+			}
 			if($email == ""){
 				$complete = false;
 				$bericht = $bericht + "Gelieve jouw e-mailadres in te vullen.\n";
@@ -204,8 +119,8 @@
 	function create_calendar_appointment($date,$time,$name,$email,$phone,$remark,$type){
 		$client = getClient();
 		$service = new Google_Service_Calendar($client);
-		$calendarId = 'dietiste.borah@gmail.com';		
-		
+		$calendarId = 'dietiste.borah@gmail.com';
+
 		if($type=="opvolg"){
 			$endTime = strtotime($time) + (30*60);
 		}
@@ -242,7 +157,7 @@
 		$client->addScope("https://mail.google.com/");
 		$client->setAuthConfig('/home/borahv1q/borah-secrets/client_secret_gmail.json');
 		$client->setAccessType('offline');
-		
+
 		// Load previously authorized credentials from a file.
 		$credentialsPath = '/home/borahv1q/borah-secrets/credentials_gmail.json';
 		if (file_exists($credentialsPath)) {
@@ -257,14 +172,14 @@
 			$client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
 			file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
 		}
-		
+
 		$service = new Google_Service_Gmail($client);
 
 		if($type=="opvolg"){
-			$strMailContent = 'Beste '. $name .'<br/><br/>hierbij bevestig ik jouw opvolgconsultatie op '.$date. ' om '.$time. '.<br/><br/>Volgende opmerkingen waren toegevoegd:<br/>'.$remark.'<br/><br/><br/>Met vriendelijke groeten,<br/><br/>Borah Van Doorslaer<br/>+32 485 36 04 09<br/>Stuiverstraat 17/1, 1840 Londerzeel';			
+			$strMailContent = 'Beste '. $name .'<br/><br/>hierbij bevestig ik jouw opvolgconsultatie op '.$date. ' om '.$time. '.<br/><br/>Volgende opmerkingen waren toegevoegd:<br/>'.$remark.'<br/><br/><br/>Met vriendelijke groeten,<br/><br/>Borah Van Doorslaer<br/>+32 485 36 04 09<br/>Stuiverstraat 17/1, 1840 Londerzeel';
 		}
 		else{
-			$strMailContent = 'Beste '. $name .'<br/><br/>hierbij bevestig ik jouw startconsultatie op '.$date. ' om '.$time. '.<br/><br/>Volgende opmerkingen waren toegevoegd:<br/>'.$remark.'<br/><br/><br/>Met vriendelijke groeten,<br/><br/>Borah Van Doorslaer<br/>+32 485 36 04 09<br/>Stuiverstraat 17/1, 1840 Londerzeel';		
+			$strMailContent = 'Beste '. $name .'<br/><br/>hierbij bevestig ik jouw startconsultatie op '.$date. ' om '.$time. '.<br/><br/>Volgende opmerkingen waren toegevoegd:<br/>'.$remark.'<br/><br/><br/>Met vriendelijke groeten,<br/><br/>Borah Van Doorslaer<br/>+32 485 36 04 09<br/>Stuiverstraat 17/1, 1840 Londerzeel';
 		}
 		$strMailTextVersion = strip_tags($strMailContent, '');
 
@@ -282,12 +197,12 @@
 		$strRawMessage .= 'To: ' . encodeRecipients($strToMailName . " <" . $strToMail . ">") . "\r\n";
 		$strRawMessage .= 'Bcc: '. encodeRecipients($strToMailNameBcc . " <" . $strToMailBcc . ">") . "\r\n";
 		$strRawMessage .= 'From: '. encodeRecipients($strSesFromName . " <" . $strSesFromEmail . ">") . "\r\n";
-		
+
 		$strRawMessage .= 'Subject: =?' . $subjectCharset . '?B?' . base64_encode($strSubject) . "?=\r\n";
 		$strRawMessage .= 'MIME-Version: 1.0' . "\r\n";
 		$strRawMessage .= 'Content-type: Multipart/Alternative; boundary="' . $boundary . '"' . "\r\n";
- 
- 
+
+
 		$strRawMessage .= "\r\n--{$boundary}\r\n";
 		$strRawMessage .= 'Content-Type: text/plain; charset=' . $charset . "\r\n";
 		$strRawMessage .= 'Content-Transfer-Encoding: 7bit' . "\r\n\r\n";
@@ -297,7 +212,7 @@
 		$strRawMessage .= 'Content-Type: text/html; charset=' . $charset . "\r\n";
 		$strRawMessage .= 'Content-Transfer-Encoding: quoted-printable' . "\r\n\r\n";
 		$strRawMessage .= $strMailContent . "\r\n";
-		
+
 		//Send Mails
 		//Prepare the message in message/rfc822
 		try {
@@ -312,6 +227,6 @@
 		} catch (Exception $e) {
 			print($e->getMessage());
 		}
-		
+
 	}
 ?>
